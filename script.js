88f19aec975eba68f0365110a7508b2dd0cdea8f
varunganeshvg <varunganeshv200@gmail.com>
2026-06-17 19:23:43 +0530

    Updated portfolio website

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-const sections = document.querySelectorAll('.reveal');
 
-function revealOnScroll() {
-  sections.forEach(section => {
-    const rect = section.getBoundingClientRect();
-    if (rect.top < window.innerHeight - 120 && rect.bottom > 120) {
-      section.classList.add('active');
-    } else {
-      section.classList.remove('active');
-    }
-  });
-}
 
-window.addEventListener('scroll', revealOnScroll);
-revealOnScroll();
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-links a");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 150;
+        const sectionHeight = section.clientHeight;
+
+        if(window.scrollY >= sectionTop){
+
+            current = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(
+            link.getAttribute("href") === "#" + current
+        ){
+            link.classList.add("active");
+        }
+
+    });
+
+});
+
